01a6c6f44e87fa68433187109a69a79483adee5e
kasper-pawlowski <kapi28000@gmail.com>
2023-03-07 16:38:15 +0100

    .

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+//// section 2 slider
 const prevBtn = document.querySelector('.section-2-mobile__prev-btn');
 const nextBtn = document.querySelector('.section-2-mobile__next-btn');
 const sliderWrapper = document.querySelector('.section-2-mobile__wrapper');
 const cards = document.querySelectorAll('.section-2-mobile__wrapper__card');
 
 let currentSlide = 0;
-const maxSlides = cards.length - 1;
-const slideWidth = cards[0].clientWidth + 10; //10px margin-right
-const sliderWidth = maxSlides * slideWidth;
+let maxSlides = cards.length - 1;
+let slideWidth = cards[0].clientWidth + 10;
+let sliderWidth = maxSlides * slideWidth;
 
 sliderWrapper.style.width = `${sliderWidth}px`;
 
-// Funkcja do aktualizowania stylów slajdów w zależności od aktualnego slajdu
 const updateSlider = () => {
     sliderWrapper.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 
@@ -27,7 +27,6 @@ const updateSlider = () => {
     }
 };
 
-// Dodanie obsługi zdarzenia kliknięcia przycisków next i prev
 nextBtn.addEventListener('click', () => {
     currentSlide++;
     updateSlider();
@@ -38,11 +37,50 @@ prevBtn.addEventListener('click', () => {
     updateSlider();
 });
 
-// Aktualizacja slidera przy załadowaniu strony
 updateSlider();
 
-///////////////////////////////////////
-// Toggle Collapse
+///// models
+const modelsPrevBtn = document.querySelector('.models__prev-btn');
+const modelsNextBtn = document.querySelector('.models__next-btn');
+const modelsWrapper = document.querySelector('.models-wrapper');
+const modelsItem = document.querySelectorAll('.models__item');
+
+let modelsCurrentSlide = 0;
+let modelsMaxSlides = modelsItem.length - 1;
+let modelsSlideWidth = modelsItem[0].clientWidth + 10;
+let modelsSliderWidth = modelsMaxSlides * modelsSlideWidth;
+
+modelsWrapper.style.width = `${modelsSliderWidth}px`;
+
+const updateModelsSlider = () => {
+    modelsWrapper.style.transform = `translateX(-${modelsCurrentSlide * modelsSlideWidth}px)`;
+
+    if (modelsCurrentSlide === 0) {
+        modelsPrevBtn.disabled = true;
+    } else {
+        modelsPrevBtn.disabled = false;
+    }
+
+    if (modelsCurrentSlide === modelsMaxSlides) {
+        modelsNextBtn.disabled = true;
+    } else {
+        modelsNextBtn.disabled = false;
+    }
+};
+
+modelsNextBtn.addEventListener('click', () => {
+    modelsCurrentSlide++;
+    updateModelsSlider();
+});
+
+modelsPrevBtn.addEventListener('click', () => {
+    modelsCurrentSlide--;
+    updateModelsSlider();
+});
+
+updateModelsSlider();
+
+///// Toggle Collapse
 const questions = document.querySelectorAll('.faq li .faq__question');
 
 questions.forEach((question) => {
@@ -59,3 +97,20 @@ columnToogleBtn.addEventListener('click', () => {
     faqQcolumnQcollapse.classList.add('active');
     columnToogleBtn.style.display = 'none';
 });
+
+///// update on resize
+const autoResize = () => {
+    if (window.innerWidth < 768) {
+        maxSlides = cards.length - 1;
+        slideWidth = cards[0].clientWidth + 10;
+        sliderWidth = maxSlides * slideWidth;
+        updateSlider();
+        modelsSliderWidth = modelsMaxSlides * modelsSlideWidth;
+        modelsMaxSlides = modelsItem.length - 1;
+        modelsSlideWidth = modelsItem[0].clientWidth + 10;
+        updateModelsSlider();
+    } else {
+        return;
+    }
+};
+window.addEventListener('resize', autoResize);
